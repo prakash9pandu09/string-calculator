57319f4698f03dba7003d6de85807efbc093967d
Jyothi Prakash <jyothi.thaduri@zeiss.com>
2024-08-23 16:54:51 +0530

    test case for sum of two numbers seperatted by comma

--- src/index.test.ts
+++ src/index.test.ts
@@ -17,8 +17,14 @@ describe("String Calculator", () => {
         const result = strCalc.add("");
         expect(result).toEqual(0);
     });
+
     it("should return number for single number given in string", () => {
         const result = strCalc.add("1");
         expect(result).toEqual(1);
+    });
+
+    it("should return sum of two numbers seperated by comma(,)", () => {
+        const result = strCalc.add("1,2");
+        expect(result).toEqual(3);
     });
 });
