6b4afbc5d490f9820c8401dbba019bc7773ace55
Jyothi Prakash <jyothi.thaduri@zeiss.com>
2024-08-23 16:13:18 +0530

    initial test setup

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ install typescript
 initialize and setup typescript
 install jest
 initialize and setup jest
+create first test
---
+++ src/index.test.ts
@@ -0,0 +1,5 @@
+describe("Initial Test Setup", () => {
+    it("initial test", () => {
+        expect("").toEqual(0);
+    });
+})
